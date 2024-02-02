dbc0d6568a186134c2db9120df25b3aac753961c
David Buesa <113169265+dbuesa@users.noreply.github.com>
2024-02-02 10:13:23 +0100

    ppuush

--- factures.js
+++ factures.js
@@ -1,6 +1,9 @@
 export class Factura{
-    constructor(num, data, nif, client, telefon, email, subtotal, dte, baseI, iva, total, pagament) {
-        this.num = num;
+    static num = 0; 
+    data; nif; client; telefon; email; subtotal; dte; baseI; iva; total; pagament;
+
+    constructor(data, nif, client, telefon, email, subtotal, dte, baseI, iva, total, pagament) {
+        this.num = Factura.num++;
         this.data = data;
         this.nif = nif;
         this.client = client;
@@ -13,4 +16,25 @@ export class Factura{
         this.total = total;
         this.pagament = pagament;
     }
+
+    static guardarFactura(factura) {
+        const factures = Factura.obtenerFacturas();
+        factures.push(factura);
+        localStorage.setItem('factures', JSON.stringify(factures));
+    }
+
+    static obtenirFactures() {
+        const facturesGuardades = localStorage.getItem('factures');
+        return facturesGuardades ? JSON.parse(facturesGuardades) : [];
+    }
+    static descarregarJson() {
+        const factures = Factura.obtenirFacturas(); 
+        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(factures));
+        const a = document.createElement('a');
+        a.setAttribute("href", dataStr);
+        a.setAttribute("download", "facturas.json");
+        document.body.appendChild(a); 
+        a.click();
+        a.remove();
+    }   
 }
--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 import ("https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js");
+import Factura from "./factura.js";
+"use strict";
 
+function init() {
+}
+
+
+$(document).ready(init);
+
+
+$(document).ready(function() {
+    // Oculta inicialmente los diálogos para asegurar que no se muestren
+    $('.dialeg').hide();
+
+    // Manejar clic en el botón "Apunta'm!"
+    $('#apuntar').click(function() {
+        // Muestra el diálogo del formulario
+        $('#menu').show();
+    });
+
+    // Manejar clic en el botón "Plats per avui"
+    $('#pendents').click(function() {
+        // Muestra el diálogo con el menú del día
+        $('#resum').show();
+    });
+
+    // Opcionalmente, manejar clic en los botones o íconos de cerrar para ocultar los diálogos
+    $('#tancar, #tancar2, #tancar3').click(function() {
+        $('.dialeg').hide(); // Esto ocultará todos los diálogos
+    });
+});
