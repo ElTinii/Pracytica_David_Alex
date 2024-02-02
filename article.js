bc87f83002b3ce7d8d3c9736a445865478be271b
David Buesa <113169265+dbuesa@users.noreply.github.com>
2024-02-02 16:30:41 +0100

    dd

--- article.js
+++ article.js
@@ -1,12 +1,11 @@
 export class Article {
-    codi; article; unitats; preu; subtotal
+    codi; article; unitats; preu;
 
-    constructor(codi, article, unitats, preu, subtotal) {
+    constructor(codi, article, unitats, preu) {
         this.codi = codi;
         this.article = article;
         this.unitats = unitats;
         this.preu = preu;
-        this.subtotal = subtotal;
     }
 }
 
